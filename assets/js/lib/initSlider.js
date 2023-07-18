7f57e4494825e52c2a34f10247d8a4436679781c
Adrienlvlemail <adrienlvl.pro@gmail.com>
2023-07-18 15:05:48 +0200

    new

--- assets/js/lib/initSlider.js
+++ assets/js/lib/initSlider.js
@@ -1,4 +1,8 @@
-function initSlider(playlist,track){
+function initSlider(playlist, track, direction,effect) {
+
+
+
+
 
     console.log("initialisation du slider");
     console.dir(playlist);
@@ -6,6 +10,12 @@ function initSlider(playlist,track){
     console.dir(playlist[track].cover);
     const slider = document.querySelector("#slider");
     // creation d'une première image imageA
+    let trackB = null;
+    if (direction /* === true */) {
+        trackB = track + 1;
+    } else {
+        trackB = playlist.length - 1;
+    }
     const imageA = document.createElement("img");
     imageA.id = "imageA";
     imageA.src = playlist[track].cover;
@@ -14,25 +24,23 @@ function initSlider(playlist,track){
     slider.append(imageA);
     const imageB = document.createElement("img");
     imageB.id = "imageB";
-    imageB.src = playlist[track+1].cover;
-    imageB.alt = playlist[track+1].author;
+    imageB.src = playlist[trackB].cover;
+    imageB.alt = playlist[trackB].author;
     imageB.style.zIndex = "0";
     slider.append(imageB);
     // creation d'une balise texte texteA
     const texteA = document.createElement("p");
     texteA.id = "texteA";
-    
     //texteA.innerText = playlist[track].title+"/"+playlist[track].author;
-    texteA.innerHTML = "<p class='title'>"+playlist[track].title+"</p><p class='author'>"
-    +playlist[track].author+"</p>";
+    texteA.innerHTML = "<p class='title'>" + playlist[track].title + "</p><p class='author'>"
+        + playlist[track].author + "</p>";
     texteA.style.zIndex = "3";
     slider.append(texteA);
     const texteB = document.createElement("p");
     texteB.id = "texteB";
-    
     //texteA.innerText = playlist[track].title+"/"+playlist[track].author;
-    texteB.innerHTML = "<p class='title'>"+playlist[track+1].title+"</p><p class='author'>"
-    +playlist[track+1].author+"</p>";
+    texteB.innerHTML = "<p class='title'>" + playlist[trackB].title + "</p><p class='author'>"
+        + playlist[trackB].author + "</p>";
     texteB.style.zIndex = "1";
     slider.append(texteB);
     // j'aimerai connaitre le height de mon imageA mais je doit 
@@ -41,55 +49,63 @@ function initSlider(playlist,track){
     // de mon image
     setTimeout(() => {
         console.dir(imageA.clientHeight);
-        slider.style.height = imageA.clientHeight+"px";
+        slider.style.height = imageA.clientHeight + "px";
     }, 500);
-
     // Toutes les 5 secondes je souhaiterais faire disparaitre 
     // l'imageA et le texteA pour faire apparaitre l'imageB et 
     // le texteB situés en dessous
-    let trackB = track+1;
+
+    //la condition if vérifie si une operation et vrai(true) ou fausse (false)
+
+
     setInterval(() => {
         //ajouter ma transition
         imageA.classList.add("trans");
         texteA.classList.add("trans");
-        imageA.classList.add("swipeLeft");
-        texteA.classList.add("swipeLeft");
+        imageA.classList.add(effect);
+        texteA.classList.add(effect);
         // j'attend la fin de ma transition (500ms) pour la suite
-        
         setTimeout(() => {
             //je commence par incrémenter track
-            if(trackB === playlist.length-1){
-                trackB = 0; 
+            if (direction === true) {
+                if (track === playlist.length - 1) {
+                    track = 0
+                } else {
+                    track++
+                }
+                if (trackB === playlist.length - 1) {
+                    trackB = 0
+                } else {
+                    trackB++
+                }
+
             } else {
-                trackB++; 
-            } 
-            if(track === playlist.length-1){
-                track = 0; 
-            } else {
-                track++;
+                if (trackB === 0) {
+                    trackB = playlist.length - 1
+                } else {
+                    trackB--
+                }
+                if (track === 0) {
+                    track = playlist.length - 1
+                } else {
+                    track--
+                }
             }
-            
-            
-
-
             imageA.src = playlist[track].cover;
             imageA.alt = playlist[track].author;
-            texteA.innerHTML = "<p class='title'>"+playlist[track].title+"</p><p class='author'>"
-            +playlist[track].author+"</p>";
+            texteA.innerHTML = "<p class='title'>" + playlist[track].title + "</p><p class='author'>"
+                + playlist[track].author + "</p>";
             //je dois retirer la transition
             imageA.classList.remove("trans");
             texteA.classList.remove("trans");
-            imageA.classList.remove("swipeLeft");
-            texteA.classList.remove("swipeLeft");
+            imageA.classList.remove(effect);
+            texteA.classList.remove(effect);
             imageB.src = playlist[trackB].cover;
             imageB.alt = playlist[trackB].author;
-            texteB.innerHTML = "<p class='title'>"+playlist[trackB].title+"</p><p class='author'>"
-            +playlist[trackB].author+"</p>";
-            
+            texteB.innerHTML = "<p class='title'>" + playlist[trackB].title + "</p><p class='author'>"
+                + playlist[trackB].author + "</p>";
         }, 500);
     }, 5000);
-
-
 }
 
-export {initSlider}
+export { initSlider }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 //lorsque j'importe un ou plusieurs éléments je le fait toujours
 //en debut de script
-import {playlist_hiphop} from "./lib/playlist_hiphop.js";
+import { playlist_hiphop } from "./lib/playlist_hiphop.js";
 import { initSlider } from "./lib/initSlider.js";
-console.log("hello");
+
 //initialisation de mes variables 
 let currentTrack = 0;
-initSlider(playlist_hiphop,currentTrack);
+console.dir(playlist_hiphop);
+console.log("hello");
+initSlider(playlist_hiphop, currentTrack, false,"fadeOut");
